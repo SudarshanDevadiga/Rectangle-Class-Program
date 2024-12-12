cc98a36ccaa71ec08e445d5eba3037acf7a6afe7
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2024-12-12 10:49:54 +0530

    Add files via upload

---
+++ rectangleclass2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle() : length(1), width(1) {}
+
+    double area() const {
+        return length * width;
+    }
+};
+
+int main() {
+    Rectangle rect;
+
+    cout << "The area of the rectangle is: " << rect.area() << endl;
+
+    return 0;
+}
